4cc09fe2dfba661a7cdd3248fb7bee7eaf33ef9c
jhdzOlalde <jhdzolalde@gmail.com>
2023-01-09 15:08:59 -0600

    index primer modificacion

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Jhdz Olalde</title>
+</head>
+<body>
+    <header>
+        <div class="titulo">
+        <h1 class="Nombre">Javier Eduardo Hernandez Olalde</h1>
+        <h2 class="Titulo">Ingeniero en redes y telecomunicaciones</h2>
+        <h2 ><a class="Descarga" href="">Descargar  de CV</a></h2>
+        </div>
+    </header>
+</body>
+</html>
